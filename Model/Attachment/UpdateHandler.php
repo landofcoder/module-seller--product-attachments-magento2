e920f8c17c3ebfeaf57ffac777a1cdfe496e5922
landofcoder <landofcoder@gmail.com>
2022-02-08 17:33:13 +0700

    updated assets

--- Model/Attachment/UpdateHandler.php
+++ Model/Attachment/UpdateHandler.php
@@ -3,10 +3,23 @@
 declare(strict_types = 1);
 
 /**
- * File: UpdateHandler.php
+ * Landofcoder
  *
- * @author Bartosz Kubicki bartosz.kubicki@lizardmedia.pl>
- * @copyright Copyright (C) 2018 Lizard Media (http://lizardmedia.pl)
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Landofcoder.com license that is
+ * available through the world-wide-web at this URL:
+ * https://landofcoder.com/license
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category   Landofcoder
+ * @package    Lofmp_ProductAttachment
+ * @copyright  Copyright (c) 2021 Landofcoder (https://landofcoder.com/)
+ * @license    https://landofcoder.com/LICENSE-1.0.html
  */
 
 namespace Lofmp\ProductAttachment\Model\Attachment;
